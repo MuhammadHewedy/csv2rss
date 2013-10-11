f430005b72d74fabc6626fa93ac799b0e35ac1db
mhewedy <mohammed_a_hewedy@hotmail.com>
2013-10-11 12:40:54 +0200

    read the rss tags from the header of csv file

--- feed_generator.rb
+++ feed_generator.rb
@@ -1,35 +1,32 @@
 require 'csv'
 
 def write_items_xml_from_file file, in_file
-    CSV.foreach(in_file) do |row|
+    CSV.foreach(in_file, {:headers => true}) do |row|
         file.puts (build_item_xml row)
     end
 end
 
 def write_items_xml_from_string file, in_string
-    CSV.parse(in_string) do |row|
+    CSV.parse(in_string, {:headers => true}) do |row|
         file.puts (build_item_xml row)
     end
 end
 
 def build_item_xml row
-    "\t<item>
-    <g:id>#{row[0]}</g:id>
-    <title>#{xml_encode(row[1])}</title>
-    <description>#{xml_encode(row[1])} by #{xml_encode(row[5])}</description>
-    <g:product_type>#{xml_encode(row[11])}</g:product_type>
-    <link>#{xml_encode(row[8])}</link>
-    <g:image_link></g:image_link>
-    <g:condition>new</g:condition>
-    <g:availability>#{if row[4] and row[4].upcase == 'Y'; 'in stock' else 'out of stock' end}</g:availability>
-    <g:price>#{xml_encode(row[9])} USD</g:price>
-    </item>\n"
-end
+    item = "\t<item>"
+    row.each do |e|
+        tag_name = e[0].strip.encode(:xml => :text) if e[0]
+        value = e[1].strip.encode(:xml => :text) if e[1]
 
-def xml_encode str
-    if str
-        str.encode(:xml => :text)
+        unless !tag_name or tag_name.empty?
+            if value and value.empty?
+                item += "\n\t<#{tag_name} />"
+            else
+                item += "\n\t<#{tag_name}>#{value}</#{tag_name}>"
+            end
+        end
     end
+    item += "\n\t</item>\n"
 end
 
 def write_pre file
